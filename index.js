50bb1f6365db8cde0250e55d85c9e47db691dc53
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-06-08 11:44:29 +0300

    hm

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Lviv&APPID=17901880cf618fa893ba9835f07915f7")
-  .then(response => response.json())
-  .then(data => document.querySelector(".weather").innerHTML = makeList(data));
+document.getElementById("check").onclick = function () {
+  const city = document.getElementById("city").value;
+  const result = document.getElementById("result");
 
-function makeList(obj) {
-    const placeholder = `<ul class="weather-list">
-        <li><p>Погода: ${obj.weather[0].main}</p></li>
-        <li><p>Кількість хмар: ${obj.clouds.all}</p></li>
-        <li><p>Вологість: ${obj.main.humidity}</p></li>
-        <li><p>Швидкість вітру: ${obj.wind.speed} км/год</p></li>
-    </ul>`;
-    return placeholder;
-}
+  if (!city) {
+    result.innerText = "Введіть назву міста!";
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2b8ac17e85bdfc23a5762f243815f217&units=metric&lang=ua`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.cod !== 200) {
+        result.innerText = "Місто не знайдено!";
+      } else {
+        result.innerHTML = `
+          <h3>${data.name}</h3>
+          <p>Температура: ${data.main.temp} °C</p>
+          <p>${data.weather[0].description}</p>
+          <p>Швидкість вітру: ${data.wind.speed} м/с</p>
+          <p>Тиск: ${data.main.pressure} гПа</p>
+        `;
+      }
+    })
+    .catch(() => {
+      result.innerText = "Помилка при завантаженні.";
+    });
+};
+
